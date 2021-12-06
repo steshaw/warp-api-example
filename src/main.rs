ed83095a13156f55e0c864dbfd028e3974050ed7
Steven Shaw <steven@steshaw.org>
2021-12-06 18:33:20 +1000

    Add some routes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use warp::Filter;
 
-fn hello() -> & 'static str {
-    "Hello, world!\n"
+fn pokemon(name: String) -> String {
+    format!("Hello Pokémon, {}\n", name)
 }
 
 #[tokio::main]
 async fn main() {
-    warp::serve(warp::any().map(hello)).run(([0, 0, 0, 0], 3000)).await;
+    // GET /pokemon/mewtwo => 200 OK with body "Hello, mewtwo!"
+    let pokemon = warp::path!("pokemon" / String)
+        .map(pokemon);
+
+    let echo = warp::path!("echo" / String)
+        .map(|echo| format!("Echo {}\n", echo));
+
+    let health = warp::path!("health").map(||"ok\n");
+
+    let default = warp::any().map(||"hmm\n");
+
+    warp::serve(pokemon.or(echo).or(health).or(default))
+        .run(([0, 0, 0, 0], 3000))
+        .await;
 }
