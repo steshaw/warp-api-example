82c58b606d71a8789752226afcefbf3af71aa01f
Steven Shaw <steven@steshaw.org>
2021-12-07 07:48:48 +1000

    Remove non-pokemon experiments

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::convert::Infallible;
 use warp::Filter;
-
-async fn placeholder() -> Result<(), reqwest::Error> {
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .post("https://jsonplaceholder.typicode.com/posts")
-        .json(&serde_json::json!({
-            "title": "Reqwest.rs",
-            "body": "https://docs.rs/reqwest",
-            "userId": 1
-        }))
-        .send()
-        .await?
-        .json()
-        .await?;
-    println!("placeholder = {:#?}", echo_json);
-    Ok(())
-}
-
-async fn your_ip() -> Result<String, Box<dyn std::error::Error>> {
-    let resp: serde_json::Value = reqwest::get("https://httpbin.org/ip").await?.json().await?;
-    println!("resp = {:#?}", resp);
-    let opt_ip = resp.get("origin");
-    println!("opt_ip = {:#?}", opt_ip);
-    match opt_ip {
-        Some(serde_json::Value::String(ip)) => Ok(ip.to_string() + "\n"),
-        Some(_) => Err("The key \"origin\" is not a string".into()),
-        None => Err("Cannot find key \"origin\"".into()),
-    }
-}
-
-async fn ip() -> Result<String, Infallible> {
-    match your_ip().await {
-        Ok(s) => Ok(s),
-        Err(err) => panic!("Argh! {}", err), // FIXME
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Habitat {
@@ -235,9 +199,6 @@ async fn pokemon(name: String) -> Result<impl warp::Reply, warp::Rejection> {
 
 #[tokio::main]
 async fn main() {
-    let r = placeholder().await;
-    println!("placeholder = {:#?}", r);
-
     if false {
         // TODO: Would make a nice integration test of the Translation API.
         // Avoid testing the Translation API as we get rate limited pretty quickly.
@@ -251,16 +212,10 @@ async fn main() {
     // GET /pokemon/mewtwo => 200 OK with body "Hello, mewtwo!"
     let pokemon = warp::path!("pokemon" / String).and_then(pokemon);
 
-    let echo = warp::path!("echo" / String).map(|echo| format!("Echo {}\n", echo));
-
-    let ip = warp::path!("ip").and_then(ip); // TODO
-
     let health = warp::path!("health").map(|| "ok\n");
 
-    let _default = warp::any().map(|| "hmm\n");
-
     warp::serve(
-        pokemon.or(echo).or(ip).or(health), //.or(default) // Disable for now otherwise not_found errors are picked up by `default`
+        pokemon.or(health),
     )
     .run(([0, 0, 0, 0], 3000))
     .await;
