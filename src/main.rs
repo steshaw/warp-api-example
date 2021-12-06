610792012d68714b6c7e253a803b14a9ca475df2
Steven Shaw <steven@steshaw.org>
2021-12-06 18:15:34 +1000

    Hello Warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+fn hello() -> & 'static str {
+    "Hello, world!\n"
+}
+
+#[tokio::main]
+async fn main() {
+    warp::serve(warp::any().map(hello)).run(([0, 0, 0, 0], 3000)).await;
 }
